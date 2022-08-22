105a73beb07e52433cbb032b11d40ff868c6b5d5
Ajay-Jaiswal <100516745+Ajay-Jaiswal@users.noreply.github.com>
2022-08-22 15:01:33 +0530

    complete

---
+++ README.md
@@ -0,0 +1,16 @@
+<div align="center">
+  <img alt="Welcome GIF" src="https://user-images.githubusercontent.com/45832621/132932084-eb809240-c9b6-44f4-a4f4-1034d9893d4a.gif" />
+</div>
+
+<h1 align="center">
+  Link → <a href="https://portfolio-react-coral.vercel.app/" target="_blank">Ali Can Bayazıt</a>
+</h1>
+
+<p align="center">
+Built with <a href="https://reactjs.org/" target="_blank">React.js</a>
+
+<p align="center">
+   Hosted with <a href="https://vercel.com/" target="_blank">Vercel</a>
+</p>
+
+![Görsel](src/assets/portfolyo.png)
---
+++ src/components/Work.jsx
@@ -0,0 +1,170 @@
+import React from 'react';
+import WorkImg from '../assets/internship.jpg';
+import WorkImg2 from '../assets/blogging-site.jpg';
+import WorkImg3 from '../assets/Url-Shortner.jpg';
+import WorkImg4 from '../assets/Shoppingcart.jpg';
+import WorkImg5 from '../assets/Book-management.jpg';
+import WorkImg6 from '../assets/Bot-telegram.jpg';
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+            Projects
+          </p>
+          <p className='py-6'>// Check out some of my recent projects</p>
+        </div>
+
+{/* Container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                College Internship App
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://github.com/Ajay-Jaiswal/project-2-internship.git' target="_blank">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${WorkImg2})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Blogging site
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://github.com/Ajay-Jaiswal/Project-1-Blog.git/' target="_blank">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${WorkImg3})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Url Shortner
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://github.com/Ajay-Jaiswal/project-4-urlshortner.git' target="_blank">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/Ajay-Jaiswal/project-4-urlshortner.git' target="_blank">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${WorkImg4})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Shopping App
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://github.com/Ajay-Jaiswal/project5-shopping-cart.git' target="_blank">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/Ajay-Jaiswal/project5-shopping-cart.git' target="_blank">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${WorkImg5})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Book Model
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://github.com/Ajay-Jaiswal/project-3-book-Management.git' target="_blank">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/Ajay-Jaiswal/project-3-book-Management.git' target="_blank">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${WorkImg6})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Telegram Boat App
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://github.com/Ajay-Jaiswal/project6-telegramBot.git' target="_blank">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/Ajay-Jaiswal/project6-telegramBot.git' target="_blank">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
